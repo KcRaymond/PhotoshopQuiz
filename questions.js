e451def5813c75b84e5792ef43d43b1ef6e8a0b5
KcRaymond <kaseyleigh1978@gmail.com>
2020-10-19 21:02:06 -0600

    added first JS functions and questions.js

---
+++ questions.js
@@ -0,0 +1,49 @@
+var questions = [
+    {
+        title: "What tool can you use to select and copy any background content of a existing picture?",
+        choices: ["Selection Tool", "Cloning Tool", "Clipping Tool", "Object Tool"],
+        answer: "Cloning Tool"
+    },
+
+    {
+        title: "What format of a picture file supports a transparent background?",
+        choices: ["Jpeg", "PDF", "PNG", "PSD"],
+        answer: "PNG"
+    },
+
+    {
+        title: "What tool is used to create a clipping path around an object?",
+        choices: ["Pen Tool", "Crop Tool", "Eraser Tool", "Pencil Tool"],
+        answer: "Pen Tool"
+    },
+
+    {
+        title: "What color mode is used for printing output to offset printers?",
+        choices: ["CMYK Color", "RGB Color", "Indexed Color", "Lab Color"],
+        answer: "CMYK Color"
+    },
+
+    {
+        title: "What color mode is used for websites and on screen display?",
+        choices: ["Lab Color", "Indexed Color", "RGB Color", "CMYK Color"],
+        answer: "RGB Color"
+    },
+
+    {
+        title: "Photoshop is a _________ based program.",
+        choices: ["Vector", "Object", "Raster", "Picture"],
+        answer: "Raster"
+    },
+
+    {
+        title: "Photoshop elements can be edited/moved/reordered on this palette.",
+        choices: ["Swatches", "Paths", "Channels", "Layers"],
+        answer: "Layers"
+    },
+
+    {
+        title: "Best practice in outputting files for print production is in what resolution?",
+        choices: ["300 dpi", "200 dpi", "72 dpi", "90 dpi"],
+        answer: "300 dpi"
+    },
+];
--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function startTimer() {
     // create questions to display
     makeQuestions();
 }
-
+// set timer function
 function setTimer() {
 
     var countdown = setInterval(function () {
@@ -38,7 +38,7 @@ function setTimer() {
         }
     }, 1000);
 }
-
+// create question generator with for loop function
 function makeQuestions() {
     questionNumber++;
     answer = questions[questionNumber].answer
@@ -55,65 +55,7 @@ function makeQuestions() {
         answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
     }
 }
+// need to generate question answer array for function
 
-// display option to enter name to scoreboard
-function displayScore() {
-    document.getElementById("quiz").classList.add('d-none');
-    document.getElementById("submit-score").classList.remove('d-none');
-    userScoreElement.textContent = "FINAL SCORE: " + secondsLeft + ".";
-}
 
-// Event Listeners for Main Buttons
-startBtn.addEventListener("click", startTimer);
-submitBtn.addEventListener("click", function (event) {
-    event.stopPropagation();
-    addScore();
-    
-    window.location.href = "./playerscores.html"
-});
-
-function addScore () {
-    userNameInput = document.getElementById("userName").value
-    
-    // create a new object with name and score keys
-var newScore = {
-        name: userNameInput,
-        score: secondsLeft
-    };
-    // check if there are scores in local storage first and take value
-    //if not, make a blank array
-    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-    // push object into score array
-    highScores.push(newScore)
-    // turn objects into an array of strings + put it into local storage
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-}
-
-function hideFeedback(){
-    var pElement = document.getElementsByClassName("feedback")[0]
-    pElement.style.display='none'
-}
-
-function showFeedback(){
-    var pElement = document.getElementsByClassName("feedback")[0]
-    pElement.removeAttribute('style');
-}
-
-answerChoices.addEventListener("click", function (event) {
-    var pElement = document.getElementsByClassName("feedback")[0]
-    
-    // evaluation of user's answer choices & feedback
-    if (answer === event.target.textContent) {   
-        pElement.innerHTML = "You got it correct! Great Job";
-        setTimeout(hideFeedback,1225);
-        showFeedback();   
-        
-    } else {
-        pElement.innerHTML = "Sorry, wrong. You will have to retake the quiz...";
-        setTimeout(hideFeedback,1225);
-        secondsLeft = secondsLeft - 10;
-        showFeedback();
-    }    
-    makeQuestions();
-});
 
